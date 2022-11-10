622c5e03cf5134eaf6c22b2f101d4ed3b0c601b4
Jamilx <99324373+Jamilx@users.noreply.github.com>
2022-11-10 19:03:41 +0200

    v1

--- bot.py
+++ bot.py
@@ -8,20 +8,10 @@ bot = Bot(token = BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
-
-
-
-
-
-
-
-
-
 @dp.message_handler()
 async def echo_send(message : types.Message):
     await message.answer(message.text)
-    await message.reply(message.text)
-    await message.bot.send_massage(message.from_user.id, "text")
+
 
 
 executor.start_polling(dp, skip_updates = True)
